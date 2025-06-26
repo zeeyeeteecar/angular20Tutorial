abaf7ea9c5eb5c57d550a3f5f4391ce0f5e55c77
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2025-06-26 11:54:44 -0700

    1

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -38,7 +38,7 @@ export class User {
   getUsers() {
     this.http
       //.get("https://api.freeprojectapi.com/api/GoalTracker/getAllUsers")
-      .get("/api/donor/donorInfo.aspx")
+      .get("https://www.accessrichmond.org/o2b2/api/donor/donorInfo.aspx")
       .subscribe((result: any) => {
         //  debugger;
         this.userList = result;
